c5e06848a4cee8c2c25155d80bd3bde85baf2ca2
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-11 19:26:03 +0100

    Update two_res_var.tf

--- two_res_var.tf
+++ two_res_var.tf
@@ -1,6 +1,6 @@
 resource "local_file" "abc_file" {
     filename = "/home/kalyan/var_abc.txt"
-    content = "Hello there, this is my new file !"
+    content = var.content
 }
 
 resource "random_pet""pet" {
