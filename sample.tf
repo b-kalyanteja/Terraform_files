3274ad01da809e6b9be5cde03ea4d185d34bc392
Ubuntu <kalyan@kalyan.04kkrxoe5g0ezoi3i0hxelmeza.lx.internal.cloudapp.net>
2024-02-11 14:49:07 +0000

    new writtern in linux

--- sample.tf
+++ sample.tf
@@ -1,4 +1,4 @@
 resource "local_file" "sample" {
     filename = "/home/kalyan/new.txt"
-    content = "Hello there, this is my new file !"
+    content = "Hello there, this is my new file updated in linux and pushed to git ! "
 }
