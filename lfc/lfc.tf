5a3b1bda68dfbfcb10623c5e8ccdfc842f821bd0
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 16:48:17 +0100

    Update lfc.tf

--- lfc/lfc.tf
+++ lfc/lfc.tf
@@ -1,6 +1,6 @@
 resource "local_file" "sample" {
     filename = "/home/kalyan/new.txt"
-    content = "Hello there, this is my new file updated in linux and pushed to git ! "
+    content = "Hello there, this is my new second updated in linux and pushed to git ! "
     lifecycle {
         create_before_destroy = true
     }
