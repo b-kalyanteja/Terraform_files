9673407af7ea45e1f5bb1423b969002f1ccb70aa
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 17:03:29 +0100

    Update lfc.tf

--- lfc/lfc.tf
+++ lfc/lfc.tf
@@ -1,10 +1,10 @@
 resource "local_file" "sample" {
-    filename = "/home/kalyan/terra/lfc/new.txt"
-    content = "Hello there, this is my new fifth updated . \n ignore_changes activated. no its not to done  "
+    filename = "/home/kalyan/terra/lfc/newbad.txt"
+    content = "Hello there, this is my new sisth updated \n updated content and file name. \n but \n ignore_changes activated for file name "
     lifecycle {
         create_before_destroy = true
         ignore_changes = [
-            content
+            filename
         ]
     }
 }
