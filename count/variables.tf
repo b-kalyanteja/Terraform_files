b0a0a9a2f8566a3b4e2f7b5ac77240d6591b2aa1
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 19:41:29 +0100

    Update variables.tf

--- count/variables.tf
+++ count/variables.tf
@@ -1,4 +1,7 @@
 variables "fn" {
   type = list
   default = [/home/kalyan/terra/lfc/xxx.txt, /home/kalyan/terra/lfc/yyy.txt, /home/kalyan/terra/lfc/zzz.txt]
+  conut = 3
 }
+
+#filename  = "/home/kalyan/terra/lfc/${element(["aaa.txt", "bbb.txt", "ccc.txt"], count.index)}"
