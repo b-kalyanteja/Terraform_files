4e34b71aaf3d4c9b0bebb51eeeff751b19cd0db5
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 16:53:54 +0100

    Update lfc.tf
    prevent drstroy

--- lfc/lfc.tf
+++ lfc/lfc.tf
@@ -1,8 +1,8 @@
 resource "local_file" "sample" {
     filename = "/home/kalyan/terra/lfc/new.txt"
     content = "Hello there, this is my new third updated in linux and pushed to git ! it will destroy foirst and then create as no life cyle given \n hi \n hi "
-    #lifecycle {
-        #create_before_destroy = true
+    lifecycle {
+        prevent_destroy = true
         #ignorechanges[]
-    #}
+    }
 }
