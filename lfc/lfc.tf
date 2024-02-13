63883295624ba88301dbcaa14adccbcdc7ad6f3c
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 18:13:51 +0100

    Update lfc.tf

--- lfc/lfc.tf
+++ lfc/lfc.tf
@@ -1,8 +1,8 @@
 resource "local_file" "sample" {
-    filename = "/home/kalyan/terra/lfc/newbadd.txt"
+    filename = "/home/kalyan/terra/lfc/apc.txt"
     content = " NOPEEEE bow bow"
     lifecycle {
         create_before_destroy = true
-        ignore_changes = all
+        #ignore_changes = all
     }
 }
