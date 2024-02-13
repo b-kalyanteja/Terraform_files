a3bfa20442f842cda57b83d17bbfb174f09e4659
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 19:58:00 +0100

    Update count.tf

--- count/count.tf
+++ count/count.tf
@@ -2,6 +2,9 @@ resource "local_file" "sample" {
     filename = var.fn[count.index]
     content = data.local_file.beta.content
     count = 3
+    lifecycle {
+    create_before_destroy = true
+    }
     
 }
 data "local_file" "beta"{
