9da4c66f14a4771983b5ff4d8ba0409ae54546c3
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 20:37:02 +0100

    Update foreach.tf

--- foreach/foreach.tf
+++ foreach/foreach.tf
@@ -2,7 +2,7 @@ resource "local_file" "sample" {
     filename = each.value
     for_each = var.fn
     content = data.local_file.beta.content
-    count = 3
+    
     lifecycle {
     create_before_destroy = true
     }
