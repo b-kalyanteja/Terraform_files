eec6148fd84a52c7129b21ec4f398af5dfaf8609
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 16:56:19 +0100

    Update lfc.tf

--- lfc/lfc.tf
+++ lfc/lfc.tf
@@ -1,6 +1,6 @@
 resource "local_file" "sample" {
     filename = "/home/kalyan/terra/lfc/new.txt"
-    content = "Hello there, this is my new third updated in linux and pushed to git ! it will destroy foirst and then create as no life cyle given \n hi \n hi "
+    content = "Hello there, this is my new fouth updated . to prevent destroy "
     lifecycle {
         prevent_destroy = true
         #ignorechanges[]
