b2d67e8ac505ed6fe49a96875c69305a2a61ab54
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 20:49:22 +0100

    Update variables.tf

--- foreach/variables.tf
+++ foreach/variables.tf
@@ -1,6 +1,6 @@
 variable "fn" {
   type = set(string)
-  default = ["/home/kalyan/terra/foreach/x.txt", "/home/kalyan/terra/foreach/y.txt", "/home/kalyan/terra/foreach/z.txt"]
+  default = ["/home/kalyan/terra/foreach/ox.txt", "/home/kalyan/terra/foreach/oy.txt", "/home/kalyan/terra/foreach/oz.txt"]
 }
 
 
