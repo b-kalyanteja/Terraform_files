a6b3f6be4ae94ee6563186920e16730dcf3c4be4
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 18:22:10 +0100

    Update lfc.tf

--- lfc/lfc.tf
+++ lfc/lfc.tf
@@ -1,11 +1,11 @@
 resource "local_file" "sample" {
     filename = "/home/kalyan/terra/lfc/apc.txt"
-    content = " NOPEEEE bow bow"
+    content = data.local_file.beta.content
     lifecycle {
         create_before_destroy = true
         #ignore_changes = all
     }
 }
-resource "local_file" "beta"{
+data "local_file" "beta"{
     filename = "/home/kalyan/aaa.txt"
     }
