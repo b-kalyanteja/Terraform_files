1f3c50f264a6ca70f58c28cd98936e24863ce9b7
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 19:45:40 +0100

    Update count.tf

--- count/count.tf
+++ count/count.tf
@@ -1,5 +1,5 @@
 resource "local_file" "sample" {
-    filename = "/home/kalyan/terra/lfc/${element(["aaa.txt", "bbb.txt", "ccc.txt"], count.index)}"
+    filename = "/home/kalyan/terra/count/${element(["aaa.txt", "bbb.txt", "ccc.txt"], count.index)}"
     content = data.local_file.beta.content
     count = 3
     
