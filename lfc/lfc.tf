d290deacad481a64281ea866da645d22459562e2
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 17:01:30 +0100

    Update lfc.tf

--- lfc/lfc.tf
+++ lfc/lfc.tf
@@ -1,6 +1,6 @@
 resource "local_file" "sample" {
     filename = "/home/kalyan/terra/lfc/new.txt"
-    content = "Hello there, this is my new fouth updated . to prevent destroy "
+    content = "Hello there, this is my new fifth updated . \n ignore_changes activated. no its not to done  "
     lifecycle {
         create_before_destroy = true
         ignore_changes = [
