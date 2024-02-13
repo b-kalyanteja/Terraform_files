d73090edab1fe0aebf82945a6292467d0b23e66d
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-13 16:57:28 +0100

    Update lfc.tf

--- lfc/lfc.tf
+++ lfc/lfc.tf
@@ -2,7 +2,6 @@ resource "local_file" "sample" {
     filename = "/home/kalyan/terra/lfc/new.txt"
     content = "Hello there, this is my new fouth updated . to prevent destroy "
     lifecycle {
-        prevent_destroy = true
         #ignorechanges[]
     }
 }
