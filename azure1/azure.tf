e61b37c62afe9751773785707ab742b56f2daad3
Calyan Teya <99665389+b-kalyanteja@users.noreply.github.com>
2024-02-15 17:34:05 +0100

    Update azure.tf

--- azure1/azure.tf
+++ azure1/azure.tf
@@ -1,12 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.7.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
-  tenant_id = "4e21a9c0-bf13-4006-95a1-b6e3b71c79eb"
+  tenant_id = "4e21a9c0-bf13-4006-95a1-b6e3b71c79eb"  
 }
 
 resource "azurerm_storage_account" "example" {
   name                     = "byandforterraform"
-  resource_group_name      = "vmware-rg"
-  location                 = "Japan East"
+  resource_group_name      = "vmware-rg" 
+  location                 = "japaneast" 
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
@@ -14,4 +23,3 @@ resource "azurerm_storage_account" "example" {
     environment = "cal production"
   }
 }
-
