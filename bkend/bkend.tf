895dd30344104559a0da6dc7cf44488b559dec0d
b-kalyanteja <b.kalyanteja@gmail.com>
2024-02-19 19:52:23 +0100

    new readme file

--- bkend/bkend.tf
+++ bkend/bkend.tf
@@ -1,8 +1,13 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "kalyandatalake"
-    container_name       = "terra"
+    storage_account_name = "byandforterraform"
+    container_name       = "statefiles"
     key                  = "terraform.tfstate"
   }
 }
 
+
+resource "local_file" "sample" {
+    filename = "D:/new.txt"
+    content = "Hello there, this is my new file updated in linux and pushed to git ! "
+}
---
+++ bkend/readme.txt
@@ -0,0 +1,4 @@
+this is to create normal resource in local computer, 
+but saves terraform fstate file in backend azure container
+
+need azure storage + container to executet this
