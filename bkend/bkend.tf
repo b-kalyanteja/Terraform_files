8396cad1edc4d767780bc93c329c7755cc3e6a32
b-kalyanteja <b.kalyanteja@gmail.com>
2024-02-19 19:58:56 +0100

    newpush

--- bkend/bkend.tf
+++ bkend/bkend.tf
@@ -3,6 +3,7 @@ terraform {
     storage_account_name = "byandforterraform"
     container_name       = "statefiles"
     key                  = "terraform.tfstate"
+    sas_token            = "?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-02-20T02:57:10Z&st=2024-02-19T18:57:10Z&spr=https,http&sig=zJtEucmeHbHrLD4QJeI4CJpAU0O77umH4%2FSV%2Bokcp8o%3D"
   }
 }
 
