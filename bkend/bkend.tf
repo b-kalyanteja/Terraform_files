f093283df5b3673e7101779b3f40c8f18f4d86ec
b-kalyanteja <b.kalyanteja@gmail.com>
2024-02-20 16:05:09 +0100

    sas for entire sotrage acconnt

--- bkend/bkend.tf
+++ bkend/bkend.tf
@@ -3,8 +3,8 @@ terraform {
     storage_account_name = "byandforterraform"
     container_name       = "statefiles"
     key                  = "terraform.tfstate"
-    sas_token            = "sp=racwdli&st=2024-02-20T14:54:49Z&se=2024-02-20T15:05:59Z&sv=2022-11-02&sr=c&sig=%2BTTxiOO0Sch98fm0QtBWDj9Ig%2BQnQFrWkBUbKrXdWQA%3D"
-}
+    sas_token            = "BlobEndpoint=https://byandforterraform.blob.core.windows.net/;QueueEndpoint=https://byandforterraform.queue.core.windows.net/;FileEndpoint=https://byandforterraform.file.core.windows.net/;TableEndpoint=https://byandforterraform.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-02-20T23:04:19Z&st=2024-02-20T15:04:19Z&spr=https,http&sig=HLADtaQ79RyKJWVTSVHmcQ17YfqY%2Bqax5580mB%2FeGqs%3D"
+  }
 }
 
 resource "local_file" "sample" {
