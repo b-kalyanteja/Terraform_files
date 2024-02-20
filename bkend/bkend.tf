46acc38fa25be82d9db4268fe9bdf79e3244baf8
b-kalyanteja <b.kalyanteja@gmail.com>
2024-02-20 16:01:01 +0100

    new token variable

--- bkend/bkend.tf
+++ bkend/bkend.tf
@@ -3,12 +3,12 @@ terraform {
     storage_account_name = "byandforterraform"
     container_name       = "statefiles"
     key                  = "terraform.tfstate"
-    sas_token            = "sp=r&st=2024-02-20T14:54:49Z&se=2024-02-20T22:59:59Z&sv=2022-11-02&sr=c&sig=xEiExDlE1HYWd5Z96qpNLywmUA7hmmLaPiLYoJ1CepA%3D"
+    sas_token            = var.token
 
 }
 }
 
 resource "local_file" "sample" {
-    filename = "D:/new.txt"
+    filename = "D:/neew.txt"
     content = var.content
 }
--- bkend/variables.tfvars
+++ bkend/variables.tfvars
@@ -2,3 +2,8 @@ variable "content" {
  type = string
  default = "my file value from variables fron variables.tfvars with lease ID taken"
 }
+
+variable "token" {
+ type = string
+ default = "sp=racwdli&st=2024-02-20T14:54:49Z&se=2024-02-20T15:05:59Z&sv=2022-11-02&sr=c&sig=%2BTTxiOO0Sch98fm0QtBWDj9Ig%2BQnQFrWkBUbKrXdWQA%3D"
+}
