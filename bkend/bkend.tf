b2a20413aa0990e7ab5fd8485ac60faa85e6aea5
b-kalyanteja <b.kalyanteja@gmail.com>
2024-02-20 16:03:20 +0100

    a

--- bkend/bkend.tf
+++ bkend/bkend.tf
@@ -3,8 +3,7 @@ terraform {
     storage_account_name = "byandforterraform"
     container_name       = "statefiles"
     key                  = "terraform.tfstate"
-    sas_token            = var.token
-
+    sas_token            = "sp=racwdli&st=2024-02-20T14:54:49Z&se=2024-02-20T15:05:59Z&sv=2022-11-02&sr=c&sig=%2BTTxiOO0Sch98fm0QtBWDj9Ig%2BQnQFrWkBUbKrXdWQA%3D"
 }
 }
 
