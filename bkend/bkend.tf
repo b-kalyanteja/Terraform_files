0c9de8c79b1fc627a45654ac75b6dd99c93037ba
b-kalyanteja <b.kalyanteja@gmail.com>
2024-02-20 15:56:08 +0100

    new SAS

--- bkend/bkend.tf
+++ bkend/bkend.tf
@@ -3,7 +3,7 @@ terraform {
     storage_account_name = "byandforterraform"
     container_name       = "statefiles"
     key                  = "terraform.tfstate"
-    sas_token            = "sp=r&st=2024-02-20T14:44:14Z&se=2024-02-20T14:50:59Z&sv=2022-11-02&sr=c&sig=s9G8goZKUBLq%2F6qQVpLToTsKBbwAJkUwKIJsrQTXA74%3D"
+    sas_token            = "sp=r&st=2024-02-20T14:54:49Z&se=2024-02-20T22:59:59Z&sv=2022-11-02&sr=c&sig=xEiExDlE1HYWd5Z96qpNLywmUA7hmmLaPiLYoJ1CepA%3D"
 
 }
 }
